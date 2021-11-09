5119d77985d273e7c0956e74e7e5814b340aa8f4
PhaPhanit <phanphanit.pp12345@gmail.com>
2021-11-09 22:40:57 +0700

    react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
+import Logo from '../images/rupp-logo.png'
+import {HiOutlineSearch} from 'react-icons/hi'
 
 function Navbar() {
     return (
-        <h1>Navbar</h1>
+        <header className="en-font">
+            <NavLink to="/" className="logo">
+                <img src={Logo} alt="" />
+                <p>RUPP</p>
+            </NavLink>
+            <ul>
+                <li>
+                    <NavLink className="color-white" to="/">
+                        Home
+                    </NavLink>
+                </li>
+                <li>Faculty</li>
+                <li>Scholarship</li>
+                <li>News</li>
+                <li>Map</li>
+                <li>Contact</li>
+                <li className="search active">
+                    <form>
+                        <input type="text" placeholder="Search" />
+                        <HiOutlineSearch className="icon" />
+                    </form>
+                </li>
+            </ul>
+            <NavLink to="/signup" className="btn btn-signup">Sign Up</NavLink>
+        </header>
     )
 }
 
